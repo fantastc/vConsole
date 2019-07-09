8143c686d20732a9f8f09f5fa7a46dfe00f7d5d2
D <1356007565@qq.com>
2019-07-09 22:12:35 +0800

    待办事项

--- doc/todo.js
+++ doc/todo.js
@@ -9,26 +9,27 @@ VMC'Vue Mobile Console',Vue项目的移动端调试工具
 
 /* 
 通过查询参数开启 ?key1=a&vmc=true 
-localStorage 开启 preserve 功能 
+
 控制开关可移动 
+  localStorage 开启 preserve 功能 
+  
+不再 scroll, 体验不好 
+
+增加Tab options 高度
 
 Console 
-  Log
-  Warn
-  Error
-  Info
+  Info 
+  样式调整 
 Element 
+  减少刷新延时
 Network 
-  Request Methods 
-  Status 
-  Time 
-  Hearders 
+  默认折叠 
+  error 事件
   可选择请求数据类型 
 Routes 
   system 
   UA 
 Storage 
-  cookie 
   localStorage
   sessionStorage
   Vuex store 
